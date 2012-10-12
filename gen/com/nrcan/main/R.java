76578bc86ebe121f05bb1951762d5c7d98b970b9
Pierre Laforest-Grant <lafo0147@algonquinlive.com>
2012-10-12 15:16:22 -0400

    adfasf

--- gen/com/nrcan/main/R.java
+++ gen/com/nrcan/main/R.java
@@ -78,13 +78,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
-        public static final int label_date=0x7f040006;
         public static final int label_easting=0x7f040008;
         public static final int label_elevation=0x7f040007;
         public static final int label_latitude=0x7f04000a;
         public static final int label_longitude=0x7f04000b;
         public static final int label_northing=0x7f040009;
-        public static final int label_traverse=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int metadata_label_cameraPrefix=0x7f040013;
         public static final int metadata_label_geologistCode=0x7f040012;
@@ -97,6 +95,8 @@ public final class R {
         /**  METADATA TABLE 
          */
         public static final int metadata_label_workspace=0x7f04000c;
+        public static final int station_label_date=0x7f040006;
+        public static final int station_label_traverse=0x7f040005;
         public static final int title_activity_main=0x7f040003;
         public static final int title_station=0x7f040004;
     }
