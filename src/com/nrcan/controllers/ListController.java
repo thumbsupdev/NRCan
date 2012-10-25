3a6ee6c410e01a0c8191ab0226b8132071665c82
Jordan <Jordan@10.50.18.151>
2012-10-25 15:30:29 -0400

    updated some files...

--- src/com/nrcan/controllers/ListController.java
+++ src/com/nrcan/controllers/ListController.java
@@ -1,15 +1,25 @@
 package com.nrcan.controllers;
 
+import com.nrcan.main.R;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.TextView;
 
 public class ListController extends BaseAdapter implements Filterable {
+	private LayoutInflater mInflater;
+	private String title;
 
+	public ListController (String title) {
+		this.title = title;
+	}
+	
 	public int getCount() {
-		return 0;
+		return 1;
 	}
 
 	public Object getItem(int arg0) {
@@ -20,12 +30,26 @@ public class ListController extends BaseAdapter implements Filterable {
 		return 0;
 	}
 
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		
+		CellNewHolder holder;
+		holder = new CellNewHolder();
+		convertView = mInflater.inflate(R.layout.cell_new, null);
+		
+		holder.textView = (TextView) convertView.findViewById(R.id.cell_new_textView2);
+		holder.textView.setText(title);
+		
+		convertView.setTag(holder);
+		
 		return null;
 	}
 
 	public Filter getFilter() {
 		return null;
+	}
+	
+	static class CellNewHolder {
+		TextView textView;
 	}
 
 }
--- src/com/nrcan/main/MainActivity.java
+++ src/com/nrcan/main/MainActivity.java
@@ -7,10 +7,8 @@ import com.nrcan.controllers.StationBedrockController;
 import com.nrcan.controllers.StructureController;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.ListActivity;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +17,7 @@ import android.widget.ViewFlipper;
 
 public class MainActivity extends ListActivity {
 	
-	private MetadataController adap1;
+	private StationBedrockController adap1;
 	private StationBedrockController adap2;
 	private SampleBedrockController adap3;
 	private StructureController adap4;
@@ -30,6 +28,29 @@ public class MainActivity extends ListActivity {
 	private ListView lv3;
 	private ListView lv4;
 	private ListView lv5;
+	private ListView lv6;
+	private ListView lv7;
+	private ListView lv8;
+	private ListView lv9;
+	private ListView lv10;
+	private ListView lv11;
+	private ListView lv12;
+	private ListView lv13;
+	private ListView lv14;
+	private ListView lv15;
+	private ListView lv16;
+	private ListView lv17;
+	private ListView lv18;
+	private ListView lv19;
+	private ListView lv20;
+	private ListView lv21;
+	private ListView lv22;
+	private ListView lv23;
+	private ListView lv24;
+	private ListView lv25;
+	private ListView lv26;
+	private ListView lv27;
+	private ListView lv28;
 	
 	SQLiteDatabase db;
 	ViewFlipper flipper;
@@ -46,26 +67,39 @@ public class MainActivity extends ListActivity {
     			flipper.showNext();
     		}
     	});
+        Button button2 = (Button) findViewById(R.id.buttonBack);
+    	button2.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			flipper.showPrevious();
+    		}
+    	});
     	
     	//initializeDatabase();
     	
     	lv1 = getListView();
+    	/*
     	lv2 = (ListView)findViewById(R.id.listStationBedrock);
     	lv3 = (ListView)findViewById(R.id.listSampleBedrock);
     	lv4 = (ListView)findViewById(R.id.listStructure);
     	lv5 = (ListView)findViewById(R.id.listPhoto);
+    	*/
     	
-    	adap1 = new MetadataController(this, this);
+    	adap1 = new StationBedrockController(this, this);
+    	/*
     	adap2 = new StationBedrockController(this, this);
     	adap3 = new SampleBedrockController(this, this);
     	adap4 = new StructureController(this, this);
     	adap5 = new PhotoController(this, this);
+    	*/
     	
     	setListAdapter(adap1);
+    	/*
     	lv2.setAdapter(adap2);
     	lv3.setAdapter(adap3);
     	lv4.setAdapter(adap4);
     	lv5.setAdapter(adap5);
+    	*/
+    	//flipper.addView(lv1);
     }
 
     @Override
