2b8fb1fe3c20b18e496d699a6a399ab778feceb6
Jordan <Jordan@10.50.18.151>
2012-10-25 13:59:07 -0400

    new files

---
+++ src/com/nrcan/controllers/ListController.java
@@ -0,0 +1,31 @@
+package com.nrcan.controllers;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+public class ListController extends BaseAdapter implements Filterable {
+
+	public int getCount() {
+		return 0;
+	}
+
+	public Object getItem(int arg0) {
+		return null;
+	}
+
+	public long getItemId(int arg0) {
+		return 0;
+	}
+
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		return null;
+	}
+
+	public Filter getFilter() {
+		return null;
+	}
+
+}
